ab4b5b733c173b5fff704ce4fc8134ae5148c294
Mohamed Maatallah <hotelsmaatallahrecemail@gmail.com>
2025-08-20 19:24:14 +0100

    Create registry.go
    Registry  for kdf algorithms

---
+++ hashes/registry.go
@@ -0,0 +1,41 @@
+package hashes
+
+import (
+	"fmt"
+	"sort"
+)
+
+type Params struct {
+	Salt []byte
+  BcryptCost int
+	ScryptN int
+	ScryptR int
+	ScryptP int
+	ArgonTime uint32
+	ArgonMemoryKB uint32
+	ArgonParallelism uint8
+}
+
+type Hasher interface {
+	Name() string
+	Hash(plain string, p Params) (string, error)
+	Compare(target string, plain string, p Params) (bool, error)
+}
+
+var registry = map[string]Hasher{}
+
+func Register(h Hasher) { registry[h.Name()] = h }
+
+func Get(name string) (Hasher, error) {
+	if h, ok := registry[name]; ok {
+		return h, nil
+	}
+	return nil, fmt.Errorf("unknown algorithm: %s", name)
+}
+
+func List() []string {
+	out := make([]string, 0, len(registry))
+	for k := range registry { out = append(out, k) }
+	sort.Strings(out)
+	return out
+}
